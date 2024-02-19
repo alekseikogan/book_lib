5c01bd4ce1b5af9f26cb38ddfe279b6e4a87ead2
Алексей Коган <alekseikogan@yandex.ru>
2024-02-19 13:24:42 +0300

    before filter order search

--- books/store/views.py
+++ books/store/views.py
@@ -1,3 +1,9 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'books.books.settings')
+django.setup()
+
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Book
